f5ea8102db2f164a0537d2191a28f43e50efecc9
balaji <balaji11042003@gmail.com>
2022-09-24 16:34:36 +0530

    Final Commit

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,14 +1,25 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
-const { all, markAsComplete, add, today } = todoList();
+const {
+  all,
+  markAsComplete,
+  add,
+  today,
+  yesterday,
+  tomorrow,
+  overdue,
+  dueToday,
+  dueLater,
+} = todoList();
 
 describe("TodoLIst Test Suite ", () => {
   beforeAll(() => {
-    add({
-      title: "Test",
-      completed: false,
-      dueDate: today,
-    });
+    add(
+      { title: "Service Vehicle", dueDate: today, completed: false },
+      { title: "Pay rent", dueDate: today, completed: true },
+      { title: "File taxes", dueDate: tomorrow, completed: false },
+      { title: "Pay electric bill", dueDate: tomorrow, completed: false }
+    );
   });
 
   test("Should add new todo", () => {
@@ -25,5 +36,29 @@ describe("TodoLIst Test Suite ", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should retrieve overdue items", () => {
+    let temp = overdue;
+    let index;
+    for (index = 0; index < temp.length; index++) {
+      expect(temp[index].dueDate).toBe(yesterday);
+    }
+  });
+
+  test("Should retrieve due today items", () => {
+    let temp = dueToday;
+    let index;
+    for (index = 0; index < temp.length; index++) {
+      expect(temp[index].dueDate).toBe(today);
+    }
+  });
+
+  test("Should retrieve due Later items", () => {
+    let temp = dueLater;
+    let index;
+    for (index = 0; index < temp.length; index++) {
+      expect(temp[index].dueDate).toBe(tomorrow);
+    }
   });
 });
